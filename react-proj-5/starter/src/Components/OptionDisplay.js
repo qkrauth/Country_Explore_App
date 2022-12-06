a0fa0e39fa2c9a492f54b87a61383b06b5e41bd2
Quinten Krauth <quintenkrauth19@gmail.com>
2022-12-06 15:59:57 -0500

    weather api added to website

--- react-proj-5/starter/src/Components/OptionDisplay.js
+++ react-proj-5/starter/src/Components/OptionDisplay.js
@@ -1,21 +1,29 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { selectPotentials } from "../redux/slices/potentialCountriesSlice";
+import { setDisplayCountry } from "../redux/slices/displayCountrySlice";
 
 const OptionDisplay = () => {
-    let currentPotentials = useSelector(selectPotentials);
-    console.log(currentPotentials);
-    return (
-        <div className="stack">
-            {currentPotentials.map((e, i) => {
-                return (
-                    <h2 key={e.name.official} className="country-option">
-                        {e.name.common}
-                    </h2>
-                );
-            })}
-        </div>
-    )
+  let currentPotentials = useSelector(selectPotentials);
+  console.log(currentPotentials);
+  const dispatch = useDispatch();
+  return (
+    <div className="stack">
+      {currentPotentials.map((e, i) => {
+        return (
+          <h2
+            key={e.name.official}
+            className="country-option"
+            onClick={() => {
+              dispatch(setDisplayCountry(currentPotentials[i]));
+            }}
+          >
+            {e.name.common}
+          </h2>
+        );
+      })}
+    </div>
+  );
 };
 
 export default OptionDisplay;
